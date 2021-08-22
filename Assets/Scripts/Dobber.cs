7b97be4e9a8c5323983ca3010695ccd033e86e1c
NIMFER <miki.magr6@gmail.com>
2021-08-22 21:59:03 +0200

    added coments to scrtipts

--- Assets/Scripts/Dobber.cs
+++ Assets/Scripts/Dobber.cs
@@ -16,11 +16,13 @@ public class Dobber : MonoBehaviour
 
     void Awake()
     {
+        //i don't know why dose need to reference it's self but it's requiered to work
         playerControls = new PlayerControls();
         //automatically find rigidbody in character
         rb = gameObject.GetComponent<Rigidbody>();
-        //lock rotation or the rigidbody
+        //lock rotation of the rigidbody
         rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX;
+        //here we enable controlls for Dobber character
         playerControls.DobberControls.Enable();
     }
 
@@ -35,6 +37,7 @@ public class Dobber : MonoBehaviour
     //this void assignes values to input virables
     void Inputs()
     {
+        //reading the Vector2 value and assigning it to moveHorizontal and moveVertical
         moveHorizontal = playerControls.DobberControls.Walk.ReadValue<Vector2>().x;
         moveVertical = playerControls.DobberControls.Walk.ReadValue<Vector2>().y;
 
@@ -42,8 +45,9 @@ public class Dobber : MonoBehaviour
 
     void Move()
     {
-
+        //new Vector3 for calculating movement values
         Vector3 move = new Vector3(moveHorizontal * 100 * moveSpeed * Time.deltaTime, rb.velocity.y, moveVertical * 100 * moveSpeed * Time.deltaTime);
+        //here we apply the movement related to the rotation of player
         rb.velocity = transform.right * move.x + transform.up * move.y + transform.forward * move.z;
 
     }
